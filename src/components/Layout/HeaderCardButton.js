38f1c691c60b608e55b8d5aba71e02a9bf5cfd0e
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2022-06-19 17:10:16 -0300

    Adicionar items ao carrinho
    - Criada lógica no CartProvider para adicionar itens ao carrinho.
    - Botão do carrinho ajustado para exibir a quantidade de itens do carrinho.
    - MealItem e MealItemForm ajustados para adicionar itens ao carrinho.

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,15 +1,22 @@
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
-
 import classes from "./HeaderCardButton.module.css";
 
 const HeaderCardButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberofCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>0</span>
+      <span className={classes.badge}>{numberofCartItems}</span>
     </button>
   );
 };
