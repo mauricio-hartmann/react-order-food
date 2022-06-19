fc8b95ef1d0c998d1f75957c1e023142bd1c74b1
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2022-06-19 16:29:40 -0300

    Contexto do carrinho
    - Criada classe de contexto do carrinho.
    - Criado provider do contexto de carrinho.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Cart from "./components/Cart/Cart";
-
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 const App = () => {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -12,13 +12,13 @@ const App = () => {
   const hideCartHandler = () => setCartIsShown(false);
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler}></Header>
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 };
 
